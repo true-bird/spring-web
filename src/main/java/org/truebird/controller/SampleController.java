881a6266776587d7a35acdd93ae22e983c9474dc
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-01 00:12:10 +0900

    add log4j.xml, RequestMapping example

--- src/main/java/org/truebird/controller/SampleController.java
+++ src/main/java/org/truebird/controller/SampleController.java
@@ -3,14 +3,26 @@ package org.truebird.controller;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
 
-    @GetMapping("/doA")
-    public void doA(){
+    @RequestMapping("")
+    public void basic() {
         log.info("doA called............");
-        log.info("----------------------");
+    }
+
+    @RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+    public void basicGet() {
+        log.info("basic get...............");
+    }
+
+    @GetMapping("/basicOnlyGet")
+    public void basicGet2() {
+        log.info("basic get only get...............");
     }
 }
