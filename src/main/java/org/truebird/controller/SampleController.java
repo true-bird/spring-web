a45e7f3016c3764b78f9dedf6a04b20bd9911d33
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-01 14:31:33 +0900

    #7 model example

--- src/main/java/org/truebird/controller/SampleController.java
+++ src/main/java/org/truebird/controller/SampleController.java
@@ -1,14 +1,18 @@
 package org.truebird.controller;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.truebird.domain.SampleDTO;
 import org.truebird.domain.SampleDTOList;
+import org.truebird.domain.TodoDTO;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,6 +20,12 @@ import java.util.Arrays;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+
+//    @InitBinder
+//    public void initBinder(WebDataBinder binder) {
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//    }
 
     @RequestMapping("")
     public void basic() {
@@ -62,6 +72,47 @@ public class SampleController {
     public String ex02Bean(SampleDTOList list) {
         log.info("list dtos: " + list);
         return "ex02Bean";
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todo) {
+        log.info("todo: " + todo);
+        return "ex03";
+    }
+
+    @GetMapping("/ex04")
+    public String ex04(SampleDTO dto, @ModelAttribute("page") int page) { // 타입에 관계없이 무조건 Model에 담아서 전
+        log.info("dto: " + dto);
+        log.info("page: " + page);
+
+        return "/sample/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public void ex05() { // WEB-INF/views/sample/ex05.jsp 호
+        log.info("/ex05..........");
+    }
+
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06() { // json 타입으로 객체 변
+        log.info("/ex06.......");
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(10);
+        dto.setName("홍길동");
+
+        return dto;
+    }
+
+    @GetMapping("/ex07")
+    public ResponseEntity<String> ex07() {
+        log.info("/ex07.........");
+
+        // {"name": "홍길동"}
+        String msg = "{\"name\": \"홍길동\"}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+        return new ResponseEntity<>(msg,headers, HttpStatus.OK);
     }
 
 
