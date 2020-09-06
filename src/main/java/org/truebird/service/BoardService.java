1d1a6908fddfc02d1b95f3cc01cca34cbd24e544
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-06 23:15:40 +0900

    #12 service implement

---
+++ src/main/java/org/truebird/service/BoardService.java
@@ -0,0 +1,18 @@
+package org.truebird.service;
+
+import org.truebird.domain.BoardVO;
+
+import java.util.List;
+
+public interface BoardService {
+
+    public void register(BoardVO board);
+
+    public BoardVO get(Long bno);
+
+    public boolean modify(BoardVO board);
+
+    public boolean remove(Long bno);
+
+    public List<BoardVO> getList();
+}
