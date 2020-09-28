3c8bc39739b935d57b6c30c269e2cfcfbc495842
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-29 02:13:39 +0900

    #21 전체 데이터의 개수 처리

--- src/main/java/org/truebird/controller/BoardController.java
+++ src/main/java/org/truebird/controller/BoardController.java
@@ -31,7 +31,10 @@ public class BoardController {
 
         log.info("list " + cri);
         model.addAttribute("list",service.getList(cri));
-        model.addAttribute("pageMaker", new PageDTO(cri, 40));
+//        model.addAttribute("pageMaker", new PageDTO(cri, 40));
+        int total = service.getTotal(cri);
+        log.info("total: " + total);
+        model.addAttribute("pageMaker", new PageDTO(cri,total));
     }
 
     @PostMapping("/register")
--- src/main/webapp/WEB-INF/views/board/list.jsp
+++ src/main/webapp/WEB-INF/views/board/list.jsp
@@ -138,7 +138,7 @@
 
         $(".move").on("click", function (e) {
             e.preventDefault();
-            actionForm.append("<input type='hidden' name='bno' value'" + $(this).attr("href")+"'>");
+            actionForm.append("<input type='hidden' name='bno' value='" + $(this).attr("href")+"'>");
             actionForm.attr("action","/board/get");
             actionForm.submit();
         })
