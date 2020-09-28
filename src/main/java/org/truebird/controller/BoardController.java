84fc1319fb26ecde173bf54a7b09f2b33b803751
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-29 01:41:45 +0900

    #21 조회 페이지로 이동, 수정과 삭제 처리

--- src/main/java/org/truebird/controller/BoardController.java
+++ src/main/java/org/truebird/controller/BoardController.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.truebird.domain.BoardVO;
 import org.truebird.domain.Criteria;
@@ -50,28 +47,37 @@ public class BoardController {
     }
 
     @GetMapping({"/get","/modify"})
-    public void get(@RequestParam("bno") Long bno, Model model) {
+    public void get(@RequestParam("bno") Long bno,
+                    @ModelAttribute("cri") Criteria cri, Model model) {
 
         log.info("/get or modify");
         model.addAttribute("board", service.get(bno));
     }
 
     @PostMapping("/modify")
-    public String modify(BoardVO board, RedirectAttributes rttr) {
+    public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri,
+                         RedirectAttributes rttr) {
         log.info("modify:  " + board);
 
         if(service.modify(board)) {
             rttr.addFlashAttribute("result", "success");
         }
+        rttr.addAttribute("pageNum", cri.getPageNum());
+        rttr.addAttribute("amout", cri.getAmount());
+
         return "redirect:/board/list";
     }
 
     @PostMapping("/remove")
-    public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+    public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri,
+                         RedirectAttributes rttr) {
         log.info("remove..." + bno);
         if (service.remove(bno)) {
             rttr.addFlashAttribute("result","success");
         }
+        rttr.addAttribute("pageNum", cri.getPageNum());
+        rttr.addAttribute("amout", cri.getAmount());
+
         return "redirect:/board/list";
     }
 
--- src/main/webapp/WEB-INF/views/board/list.jsp
+++ src/main/webapp/WEB-INF/views/board/list.jsp
@@ -42,7 +42,9 @@
                         <tr>
                             <td><c:out value="${board.bno}"/></td>
                             <td>
-                                <a href="/board/get?bno=<c:out value='${board.bno}'/>"><c:out value="${board.title}"/></a>
+                                <a class="move" href="<c:out value='${board.bno}'/>">
+                                    <c:out value="${board.title}"/>
+                                </a>
                             </td>
                             <td><c:out value="${board.writer}"/></td>
                             <td><fmt:formatDate pattern="yyyy-MM-dd" value="${board.regdate}"/></td>
@@ -134,6 +136,12 @@
             actionForm.submit();
         })
 
+        $(".move").on("click", function (e) {
+            e.preventDefault();
+            actionForm.append("<input type='hidden' name='bno' value'" + $(this).attr("href")+"'>");
+            actionForm.attr("action","/board/get");
+            actionForm.submit();
+        })
     });
 </script>
 
