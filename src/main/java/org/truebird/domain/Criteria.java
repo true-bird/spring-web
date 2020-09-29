ece63b00494ad6de492578fee4d64abfad4e8a31
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-09-29 15:03:06 +0900

    #21 검색 조건 처리를 위한 Criteria의 변화

--- src/main/java/org/truebird/domain/Criteria.java
+++ src/main/java/org/truebird/domain/Criteria.java
@@ -13,6 +13,9 @@ public class Criteria {
     private int pageNum;
     private int amount;
 
+    private String type;
+    private String keyword;
+
     public Criteria() {
         this(1,10);
     }
@@ -20,6 +23,10 @@ public class Criteria {
     public Criteria(int pageNum, int amount) {
         this.pageNum = pageNum;
         this.amount = amount;
+    }
+
+    public String[] getTypeArr() {
+        return type == null? new String[] {}: type.split("");
     }
 
 }
