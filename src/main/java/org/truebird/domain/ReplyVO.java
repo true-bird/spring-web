06b54f649880da0231b6b92bd5204a1ea7803673
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-10-06 17:10:31 +0900

    #27 댓글 처리를 위한 영속 영역

---
+++ src/main/java/org/truebird/domain/ReplyVO.java
@@ -0,0 +1,17 @@
+package org.truebird.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ReplyVO {
+
+    private Long rno;
+    private Long bno;
+
+    private String reply;
+    private String replyer;
+    private Date replyDate;
+    private Date updateDate;
+}
