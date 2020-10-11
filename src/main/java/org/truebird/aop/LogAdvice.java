3aa7a0b7723000d75e1eee281bc7d2bbd36f8aeb
sparrow.key <sparrow.key@kakaoenterprise.com>
2020-10-12 00:46:31 +0900

    #29 @Around와 ProceedingJoinPoint

--- src/main/java/org/truebird/aop/LogAdvice.java
+++ src/main/java/org/truebird/aop/LogAdvice.java
@@ -1,10 +1,14 @@
 package org.truebird.aop;
 
 import lombok.extern.log4j.Log4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Log4j
@@ -27,6 +31,28 @@ public class LogAdvice {
     public void logException(Exception exception) {
         log.info("Exception.....!!!!");
         log.info("exception: " + exception);
+    }
+
+    // 직접 메소드를 실행할 수 있는 권한을 가지며 메소드의 전후 처리 가능
+    @Around("execution(* org.truebird.service.SampleService*.*(..))") // 대상이 되는 타켓이나 파라미터 파악, 실행 결정
+    public Object logTime(ProceedingJoinPoint pjp) { // void가 아닌 메소드의 실행 결과를 반환
+        long start = System.currentTimeMillis();
+
+        log.info("Target: " + pjp.getTarget().toString());
+        log.info("Param: " + Arrays.toString(pjp.getArgs()));
+
+        // invoke method
+        Object result = null;
+
+        try {
+            result = pjp.proceed(); // 직접 실행
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+
+        log.info("TIME: " + (end - start));
+        return result;
     }
 
 }
